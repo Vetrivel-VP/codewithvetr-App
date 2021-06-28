e87c78a4a65eb05d8f27c688dc9b73a3c2dbdb92
VETRIVEL <vetrivel.galaxy@gmail.com>
2021-06-28 13:07:18 +0300

    changes : UI Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,13 +25,17 @@ let suggestions = [
   "What does CSS stands for?",
 ];
 
+var currentLI = 0;
 const searcWrapper = document.querySelector(".searchInput");
 const inputBox = searcWrapper.querySelector("input");
 const suggestionBox = searcWrapper.querySelector(".automCom_box");
+const course_search = document.getElementById("course_search");
+const rightSide_container = document.querySelector(".rightSide_container");
+const main_container = document.querySelector(".main");
 
 inputBox.onkeyup = (e) => {
   let userValue = e.target.value;
-  let emptyArray = [];
+  var emptyArray = [];
   if (userValue) {
     emptyArray = suggestions.filter((data) => {
       // filtering array value and converting user data into lowecase and
@@ -44,21 +48,53 @@ inputBox.onkeyup = (e) => {
     });
     searcWrapper.classList.add("active"); //show autosuggestions
     showSuggestions(emptyArray);
-    let allList = suggestionBox.querySelectorAll("li");
-    allList.forEach((element) => {
+
+    if (e.keyCode == 38 || e.keyCode == 40) {
+      var listItems = suggestionBox.querySelectorAll("li");
+
+      listItems[currentLI].classList.add("highlight");
+
+      // Set up a key event handler for the document
+      // Check for up/down key presses
+      switch (e.keyCode) {
+        case 38: // Up arrow
+          // Remove the highlighting from the previous element
+          listItems[currentLI].classList.remove("highlight");
+
+          currentLI = currentLI > 0 ? --currentLI : 0; // Decrease the counter
+          listItems[currentLI].classList.add("highlight"); // Highlight the new element
+          break;
+        case 40: // Down arrow
+          // Remove the highlighting from the previous element
+          listItems[currentLI].classList.remove("highlight");
+          console.log(currentLI);
+          currentLI =
+            currentLI < listItems.length - 1
+              ? ++currentLI
+              : listItems.length - 1; // Increase counter
+          console.log(currentLI);
+          listItems[currentLI].classList.add("highlight"); // Highlight the new element
+          break;
+      }
+    }
+
+    listItems.forEach((element) => {
       element.setAttribute("onclick", "selectSuggestElement(this)");
     });
   } else {
+    suggestionBox.innerHTML = "";
     searcWrapper.classList.remove("active"); //remove autosuggestions
   }
 };
 
+// Suggestion Element
 const selectSuggestElement = (element) => {
   let selectUserData = element.innerText;
   inputBox.value = selectUserData;
   console.log(selectUserData);
 };
 
+// show suggestions
 const showSuggestions = (list) => {
   let listdata;
   if (!list.length) {
@@ -69,3 +105,21 @@ const showSuggestions = (list) => {
   }
   suggestionBox.innerHTML = listdata;
 };
+
+//close on click outside
+const suggestOutsideClick = (e) => {
+  if (e.target == rightSide_container || e.target == main_container) {
+    searcWrapper.classList.remove("active");
+  }
+};
+
+window.addEventListener("click", suggestOutsideClick);
+
+// show on click inside the textbox
+const showSuggestionAgain = (e) => {
+  if (suggestionBox.innerHTML) {
+    searcWrapper.classList.add("active");
+  }
+};
+
+course_search.addEventListener("click", showSuggestionAgain);
