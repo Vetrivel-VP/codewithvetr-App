264f0a49421ada5df8fa1fabae64a70efc948595
VETRIVEL <vetrivel.galaxy@gmail.com>
2021-08-01 18:44:00 +0300

    Changes : ADMIN UI Updated

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -2,8 +2,13 @@ let admindropeMenu_btn = document.getElementById("admindropeMenu_btn");
 let adminDropMenuContainer = document.getElementById("adminDropMenuContainer");
 
 admindropeMenu_btn.addEventListener("click", () => {
-  admindropeMenu_btn.style.transform = "rotate(540deg)";
-  adminDropMenuContainer.style.display = "block";
+  if (adminDropMenuContainer.style.display == "block") {
+    admindropeMenu_btn.style.transform = "rotate(0deg)";
+    adminDropMenuContainer.style.display = "none";
+  } else {
+    admindropeMenu_btn.style.transform = "rotate(540deg)";
+    adminDropMenuContainer.style.display = "block";
+  }
 });
 
 adminDropMenuContainer.addEventListener("click", (e) => {
