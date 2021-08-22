e2fbae1a0164af6ae999597ca29bf40b564d4eb2
VETRIVEL <vetrivel.galaxy@gmail.com>
2021-08-22 19:09:13 +0300

    Chnages: Small Minor UI Bugs Updated

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,6 @@
 var API_BASE_URL =
-  "http://localhost:5001/codewithvetriapi-c56e3/us-central1/app";
-//   "https://us-central1-codewithvetriapi-c56e3.cloudfunctions.net/app";
+  //   "http://localhost:5001/codewithvetriapi-c56e3/us-central1/app";
+  "https://us-central1-codewithvetriapi-c56e3.cloudfunctions.net/app";
 
 var courses = [];
 async function loadCourses() {
